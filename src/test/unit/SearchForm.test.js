f3bea882538f8355ada2b4fd1c1ae8d78d094718
Jamie Rushford <jar.rushford@gmail.com>
2018-12-02 12:02:29 -0700

    Add Search Form Test

--- src/test/unit/SearchForm.test.js
+++ src/test/unit/SearchForm.test.js
@@ -1,29 +1,35 @@
 import React from 'react';
 import SearchForm from '../../SearchForm';
-import { mount } from 'enzyme'
+import { shallow, mount } from 'enzyme'
 
 describe('Search Form', () => {
   it('should run handleInputUpdate when clicked', () => {
     const inputMock = jest.fn()
-    const wrapper = mount(<SearchForm handleInputUpdate={inputMock} />)
+    const wrapper = shallow(<SearchForm handleInputUpdate={inputMock} />)
     wrapper.find('input').simulate('change')
     expect(inputMock).toBeCalled()
   })
 
   it('should toggle showAll when Show All button is clicked', () => {
     const showAllMock = jest.fn()
-    const wrapper = mount (<SearchForm toggleShowAll={showAllMock} />)
+    const wrapper = mount(<SearchForm toggleShowAll={showAllMock} />)
     wrapper.find('.show-all').simulate('click')
     expect(showAllMock).toBeCalled()
   })
 
   it('should change button text when clicked', () => {
     const showAllMock = jest.fn()
-    const wrapper = mount (<SearchForm toggleShowAll={showAllMock} />)
+    const wrapper = mount(<SearchForm toggleShowAll={showAllMock} />)
     wrapper.find('.show-all').simulate('click')
     expect(wrapper.find('.show-all').instance().innerText).toEqual('Show All')
     wrapper.find('.show-all').simulate('click')
     expect(wrapper.find('.show-all').instance().innerText).toEqual('Show Less')
+  })
+
+  it('should select a data set when an option is clicked on', () => {
+    const selectMock = jest.fn()
+    const wrapper = shallow(<SearchForm selectData={selectMock} />)
+    wrapper.find('select').simulate('change', {target: {value: 'kinderData'}})
   })
 
 })
